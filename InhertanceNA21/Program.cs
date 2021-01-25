d17611400f9888195fba532e4a68f1697c4125f1
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2021-01-25 14:45:26 +0100

    Add project files.

---
+++ InhertanceNA21/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InhertanceNA21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
