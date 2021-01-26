c7b344c3c42d7fbcec6cefe8de8301cbbba1473b
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2021-01-26 16:18:46 +0100

    Add Comments

--- InhertanceNA21/Program.cs
+++ InhertanceNA21/Program.cs
@@ -8,23 +8,55 @@ namespace InhertanceNA21
         static void Main(string[] args)
         {
             FuelVehicle fuelVehicle = new FuelVehicle("Volvo", 100, "ABC123");
-            Vehicle vehicle = new Vehicle("Volvo", "ABC123");
-            Vehicle vehicle2 = new FuelVehicle("Volvo", 100,  "ABC123");
+            Vehicle vehicle1 = new Vehicle("Volvo", "ABC123");
+            Vehicle vehicle2 = new FuelVehicle("Volvo", 100, "ABC123");
 
             fuelVehicle.FuelLevel = 50;
             double level = fuelVehicle.FuelLevel;
 
-            List<IDrive> vehicles = new List<IDrive>();
-            vehicles.Add(fuelVehicle);
-            vehicles.Add(vehicle);
-            vehicles.Add(vehicle2);
-
-            foreach (var v in vehicles)
+            List<IDrive> vehicles = new List<IDrive>
             {
-                Console.WriteLine(v.Drive(23));
-               // Console.WriteLine(v.Turn());
+                fuelVehicle,
+                vehicle1,
+                vehicle2,
+                new FuelCar(),
+                new FuelCar("Saab", 100, "dad234"),
+                new Bicycle("adfaf345w3")
+            };
+
+            foreach (IDrive vehicle in vehicles)
+            {
+                Console.WriteLine(vehicle.Drive(23));
+
+                //Null om casten inte fungerar
+                FuelCar abv1 = vehicle as FuelCar;
+
+                if (abv1 != null)
+                {
+                    Console.WriteLine(abv1.Turn());
+                }
+
+                //?. Nullcheck
+                Console.WriteLine(abv1?.Turn());
+
+
+                //Är instansen vehicle en FuelCar?
+                if (vehicle is FuelCar)
+                {
+                    //Unsafe cast Exception om den inte fungerar
+                    FuelCar abV2 = (FuelCar)vehicle;
+                    Console.WriteLine(abV2.Turn());
+                }
+
+                //Är instansen vehicle en FuelCar?
+                //Om ja gör även en cast och tilldela resultatet tiil varibeln fuelCar
+                if (vehicle is FuelCar fuelCar)
+                {
+                    Console.WriteLine(fuelCar.Turn());
+                }
+
             }
-            
+
         }
     }
 }
--- InhertanceNA21/Vehicle.cs
+++ InhertanceNA21/Vehicle.cs
@@ -5,23 +5,38 @@ using System.Text;
 namespace InhertanceNA21
 {
 
+    //Interface
+    //En klass kan implementera (ärva) från många interface
+    //Allt är publikt
+    //Måste Implementeras i ärvda klasser
+    //(Kan ha implementation from c# 8)
+    //Kan inte implementeras - ej skapa objekt av med new
     public interface IDrive
     {
         string Drive(double distance);
     }
 
+    //Abstrakt
+    //Kan inte implementeras - ej skapa objekt av med new
+    //Kan inehålla en blandnig av vanliga metoder och abstrakta metoder utan implementation
+    //Alla abstrakta medlemmar måste implemneteras av dem som ärver från den abstrakta klassen
+    //Kan hålla privata fält
     abstract class AbstractVehicle : IDrive
     {
+        //Virtual - En metod som markeras med nykelordet virtual är ok att skriva en ny implementation i  ärvda klasser
         public virtual string Drive(double distance)
         {
             return $"{this.GetType().Name} wants to drive for {distance} km";
         }
 
+        //Håller ingen implementation måste implementeras i ärvda klasser
         public abstract string Turn();
     }
 
+    //Vehicle ärver från AbstractVehicle
     class Vehicle : AbstractVehicle
     {
+        protected string protektedString;
         public string Brand { get; set; }
         public string RegNo { get; set; }
 
@@ -31,16 +46,19 @@ namespace InhertanceNA21
             RegNo = regNo;
         }
 
+        //Overide egen implementation av Turn
         public override string Turn()
         {
             return "Vehicle turns";
         }
     }
 
-    class FuelVehicle : Vehicle
+     class FuelVehicle : Vehicle
     {
+        //Fält
         private double fuelLevel;
 
+        //Properties
         public double FuelLevel 
         {
             get
@@ -56,19 +74,24 @@ namespace InhertanceNA21
 
         public double FuelCapacity { get; }
 
+        //Konstruktor
         public FuelVehicle(string brand, double fuelCapacity, string regNo) :base(brand, regNo)
         {
             FuelCapacity = fuelCapacity;
         }
+
+        //Metoder
 
        
     }
 
     class FuelCar : FuelVehicle
     {
+        //Const måste tilldelas värde kan inte ändras
         private const double fuelConsumption = 0.5;
         public double MaxDistance => FuelLevel / fuelConsumption;
 
+        //Kedjade konstruktorer. base anropar basklassens konstruktor, this anropar den andra konstruktorn
         public FuelCar(string brand, double fuelCapacity, string regNo) : base(brand, fuelCapacity, regNo)
         {
         }
@@ -78,19 +101,49 @@ namespace InhertanceNA21
 
         }
 
-        public override string Drive(double distance)
+        public  override string Drive(double distance)
         {
-            return base.Drive(distance);
+            var result = new StringBuilder();
+
+            result.AppendLine(base.Drive(distance));
+
+            if(distance < 0)
+            {
+                distance = 0;
+                result.AppendLine("Negative distance is assumed to be 0");
+            }
+
+           // FuelLevel = FuelLevel - (distance * fuelConsumption);
+            FuelLevel -= distance * fuelConsumption;
+
+            result.AppendLine($"RegNo : {RegNo} drove {distance}km");
+
+            return result.ToString();
         }
+
+        public string Sound() => "Tut Tut";
+
 
 
     }
 
     class Bicycle : AbstractVehicle
     {
+        public string FrameNumber { get;  }
+
+        public Bicycle(string frameNumber)
+        {
+            FrameNumber = frameNumber;
+        }
+
         public override string Turn()
         {
-            throw new NotImplementedException();
+            return "Bicycle turns";
+        }
+
+        public override string Drive(double distance)
+        {
+            return "Bicycle starts pedaling";
         }
     }
 
